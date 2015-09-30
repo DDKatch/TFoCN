9c43a3fc41ff167b37c8f2b3b7a8b19fca484d9e
DDKatch <daniil.field@yandex.ru>
2015-09-30 15:12:28 +0300

    add 2 lab

--- lab1/serial_port_get.rb
+++ lab1/serial_port_get.rb
@@ -1,9 +1,9 @@
 require 'rubyserial'
 
-com2 = Serial.new '/dev/tnt1', 57600
+com2 = Serial.new '/dev/tnt3'
 
 while true
-    inp_msg = com2.read(1)
+    inp_msg = com2.read(4)
     if inp_msg.empty?; sleep(1) 
 	else puts inp_msg end
 end
--- lab1/serial_port_send.rb
+++ lab1/serial_port_send.rb
@@ -1,6 +1,6 @@
 require 'rubyserial'
 
-com1 = Serial.new '/dev/tnt0', 57600
+com1 = Serial.new '/dev/tnt2'
 
 while true
 	com1.write STDIN.gets.chomp
